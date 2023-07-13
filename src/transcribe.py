beffb587f5f0e43516353394b50e7674a4bccc2f
keelezibel <letang.boon@gmail.com>
2023-07-13 13:18:14 +0800

    add timestamps for cpu model

--- src/transcribe.py
+++ src/transcribe.py
@@ -1,17 +1,17 @@
 import os
 import re
-import json
+import time
 import torch
 import ffmpeg
 import srt
 import shutil
+import numpy as np
+from numpy.typing import NDArray
 import datetime
-import urllib.request
 import tensorflow as tf
 from tqdm import tqdm
 from glob import glob
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
@@ -69,7 +69,43 @@ class Transcribe:
         else:
             from whispercpp import Whisper
 
-            self.whisper_model = Whisper.from_pretrained(os.getenv("WHISPERCPP_MODEL_SIZE"))
+            def to_time(n):
+                return time.strftime("%H:%M:%S", time.gmtime(int(n / 100)))
+
+            def transcribe_with_time(
+                self, data: NDArray[np.float32], num_proc: int = 1, strict: bool = False
+            ):
+                if strict:
+                    assert (
+                        self.context.is_initialized
+                    ), "strict=True and context is not initialized. Make sure to call 'context.init_state()' before."
+                else:
+                    if (
+                        not self.context.is_initialized
+                        and not self._context_initialized
+                    ):
+                        self.context.init_state()
+                        self._context_initialized = True
+
+                self.context.full_parallel(self.params, data, num_proc)
+                return [
+                    (
+                        self.context.full_get_segment_start(i),
+                        self.context.full_get_segment_end(i),
+                        # to_time(self.context.full_get_segment_start(i)),
+                        # to_time(self.context.full_get_segment_end(i)),
+                        self.context.full_get_segment_text(i),
+                    )
+                    for i in range(self.context.full_n_segments())
+                ]
+
+            Whisper.transcribe_with_time = transcribe_with_time
+
+            self.whisper_model = Whisper.from_pretrained(
+                os.getenv("WHISPERCPP_MODEL_SIZE")
+            )
+            self.whisper_model.params.with_token_timestamps(True)
+
             print("Whisper Model loaded without GPU")
 
     def source_separate(self, audio_path):
@@ -119,7 +155,7 @@ class Transcribe:
             print("stderr:", e.stderr.decode("utf8"))
             raise e
 
-    def run_whisper_cpu(self, out_path):
+    def run_whisper_cpu(self, out_path, num_proc: int = 1, strict: bool = False):
         print("Running VAD...")
 
         (
@@ -183,15 +219,46 @@ class Transcribe:
         # Run Whisper on each audio chunk
         print("Running Whisper...")
         subs = []
-
+        sub_index = 1
         for i in range(len(u)):
             for x in range(self.max_attempts):
-                result = self.whisper_model.transcribe_from_file(
-                    f"{vad_chunks_path}/" + str(i) + ".wav"
-                )
+                try:
+                    y, _ = (
+                        ffmpeg.input(f"{vad_chunks_path}/" + str(i) + ".wav", threads=0)
+                        .output(
+                            "-",
+                            format="s16le",
+                            acodec="pcm_s16le",
+                            ac=1,
+                            ar=os.getenv("SAMPLING_FREQUENCY"),
+                        )
+                        .run(
+                            cmd=["ffmpeg", "-nostdin"],
+                            capture_stdout=True,
+                            capture_stderr=True,
+                        )
+                    )
+                except ffmpeg.Error as e:
+                    raise RuntimeError(
+                        f"Failed to load audio: {e.stderr.decode()}"
+                    ) from e
+
+                arr = np.frombuffer(y, np.int16).flatten().astype(np.float32) / 32768.0
+                result = self.whisper_model.transcribe_with_time(arr)
+
                 if x + 1 < self.max_attempts:
                     print("Retrying chunk", i)
-                subs.append(result)
+            for r in result:
+                # Add to SRT list
+                subs.append(
+                    srt.Subtitle(
+                        index=sub_index,
+                        start=datetime.timedelta(seconds=r[0]),
+                        end=datetime.timedelta(seconds=r[1]),
+                        content=r[2].strip(),
+                    )
+                )
+                sub_index += 1
 
         # Write file
         # Removal of garbage lines
@@ -226,8 +293,8 @@ class Transcribe:
         clean_subs = list()
         last_line_garbage = False
         for i in range(len(subs)):
-            c = subs[i]
-            print(c)
+            c = subs[i].content
+
             c = (
                 c.replace(".", "")
                 .replace(",", "")
@@ -279,7 +346,7 @@ class Transcribe:
                 clean_subs.append(subs[i])
             last_line_garbage = is_garbage
         with open(out_path, "w", encoding="utf8") as f:
-            f.write(" ".join(clean_subs))
+            f.write(srt.compose(clean_subs))
         print("\nDone! Subs written to", out_path)
 
     def run_whisper(self, out_path):
@@ -568,13 +635,14 @@ class Transcribe:
                 self.run_whisper(out_path)
                 self.convert_srt_txt(out_path, output_txt_file)
             else:
-                out_path = os.path.join(out_path_folder, f"{filename}.txt")
+                out_path = os.path.join(out_path_folder, f"{filename}.srt")
                 self.run_whisper_cpu(out_path)
+                self.convert_srt_txt(out_path, output_txt_file)
 
-            # # Move file to processed
-            # shutil.move(
-            #     audio_path, os.path.join(self.processed_folder, f"{filename}.wav")
-            # )
+            # Move file to processed
+            shutil.move(
+                audio_path, os.path.join(self.processed_folder, f"{filename}.wav")
+            )
 
             # Clean up vad_chunks folder
             vad_chunks_dir = os.path.join(self.data_folder, "vad_chunks")
